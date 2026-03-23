cb4f2c0a44895aeb6cee3831a4f03c9e8c69b219
Christian Dähne <christiandaehne@gmx.net>
2026-03-23 16:43:08 +0100

    solved ex00

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -11,17 +11,16 @@ int	main(int argc, char *argv[])
 		std::cout << "* LOUD AND UNBEARABLE NOISE *" << std::endl;
 		return (0);
 	}
-	i = 0;
+	i = 1;
 	while (i < argc)
 	{
 		j = 0;
 		while (argv[i][j])
 		{
-			if (std::islower(static_cast<unsigned char>(argv[i][j])))
-				argv[i][j] = std::toupper(static_cast<unsigned char>(argv[i][j]));
+			std::cout << static_cast<char>(
+				std::toupper(static_cast<unsigned char>(argv[i][j])));
 			j++;
 		}
-		std::cout << argv[i][j];
 		i++;
 	}
 	std::cout << std::endl;
