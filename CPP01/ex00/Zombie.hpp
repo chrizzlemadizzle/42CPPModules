a0b30f1533fd2ccb4881306ebb4932304f876b51
Christian Dähne <christiandaehne@gmx.net>
2026-03-27 09:51:02 +0100

    started CPP01

---
+++ CPP01/ex00/Zombie.hpp
@@ -0,0 +1,16 @@
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+
+#include <string>
+#include <iostream>
+
+class Zombie
+{
+	private:
+		std::string	name;
+	public:
+		Zombie(std::string name);
+		void	announce(void) { std::cout << name << ": " << "BraiiiiiiinnnzzzZ...\n"; };
+};
+
+#endif
